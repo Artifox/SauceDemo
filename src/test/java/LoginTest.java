1bfdc562ed48df5059fad2388421fc4b13e20b6a
Artsiom Koptsiukh <sid93.ramsey@gmail.com>
2021-02-01 22:13:45 +0300

    Fix login tests

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -9,6 +9,8 @@ import java.util.concurrent.TimeUnit;
 public class LoginTest {
 
     private static final String URL = "https://www.saucedemo.com/";
+    private static final String VALID_NAME = "standard_user";
+    private static final String VALID_PASSWORD = "secret_sauce";
 
     @Test
     public static void validLogin() {
@@ -17,8 +19,23 @@ public class LoginTest {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get(URL);
+        driver.findElement(By.id("user-name")).sendKeys(VALID_NAME);
+        driver.findElement(By.id("password")).sendKeys(VALID_PASSWORD);
+        driver.findElement(By.id("login-button")).click();
+        String result = driver.findElement(By.className("product_label")).getText();
+        Assert.assertEquals(result, "Products");
+        driver.quit();
+    }
+
+    @Test
+    public static void invalidLogin() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(URL);
         driver.findElement(By.id("user-name")).sendKeys("John");
-        driver.findElement(By.id("password")).sendKeys("Doe");
+        driver.findElement(By.id("password")).sendKeys("1234567890");
         driver.findElement(By.id("login-button")).click();
         String result = driver.findElement(By.cssSelector("h3[data-test]")).getText();
         Assert.assertEquals(result, "Epic sadface: Username and password do not match any user in this service");
