baff4d710b9e4237d722aaebba500667485d9e1b
Artsiom Koptsiukh <sid93.ramsey@gmail.com>
2021-02-04 10:34:47 +0300

    1. Improving of addGoodsToBasket method 2. Add UtilityWebDriver class

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -11,14 +11,15 @@ public class LoginTest {
     private static final String URL = "https://www.saucedemo.com/";
     private static final String VALID_NAME = "standard_user";
     private static final String VALID_PASSWORD = "secret_sauce";
+    private static final String ADD_TO_CART_BUTTON_XPATH = "//*[text()='%s']/ancestor::div[@class='inventory_item']//button";
+    private static final String PRODUCT_IN_BASKET_XPATH = "//div/child::a/child::div[text()='%s']";
+    private static final String PRODUCT_NAME1 = "Sauce Labs Backpack";
+    private static final String PRODUCT_NAME2 = "Sauce Labs Bike Light";
+    private static final String PRODUCT_ABSENT_ERROR_MESSAGE = "Element isn't present in basket";
 
     @Test
     public static void validLogin() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-        driver.get(URL);
+        WebDriver driver = UtilityWebDriver.open(URL);
         login(driver, VALID_NAME, VALID_PASSWORD);
         String result = driver.findElement(By.className("product_label")).getText();
         Assert.assertEquals(result, "Products", "Login failed");
@@ -27,11 +28,7 @@ public class LoginTest {
 
     @Test
     public static void invalidLogin() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-        driver.get(URL);
+        WebDriver driver = UtilityWebDriver.open(URL);
         login(driver, "John", "1234567890");
         String result = driver.findElement(By.cssSelector("h3[data-test]")).getText();
         Assert.assertEquals(result, "Epic sadface: Username and password do not match any user in this service", "There is no warning message");
@@ -40,21 +37,17 @@ public class LoginTest {
 
     @Test
     public static void addGoodsToBasket() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-        driver.get(URL);
+        WebDriver driver = UtilityWebDriver.open(URL);
         login(driver, VALID_NAME, VALID_PASSWORD);
-        driver.findElement(By.xpath("//*[text()='Sauce Labs Backpack']/ancestor::div[@class='inventory_item']//button")).click();
-        driver.findElement(By.xpath("//*[text()='Sauce Labs Bike Light']/ancestor::div[@class='inventory_item']//button")).click();
-        driver.findElement(By.cssSelector("[data-icon=shopping-cart]")).click();
+        driver.findElement(By.xpath(String.format(ADD_TO_CART_BUTTON_XPATH, PRODUCT_NAME1))).click();
+        driver.findElement(By.xpath(String.format(ADD_TO_CART_BUTTON_XPATH, PRODUCT_NAME2))).click();
+        driver.findElement(By.id("shopping_cart_container")).click();
         int amountOfElementsInBasket = driver.findElements(By.cssSelector("[class='cart_item']")).size();
-        Assert.assertEquals(amountOfElementsInBasket, 2);
-        String result = driver.findElement(By.xpath("//div/child::a/child::div[text()='Sauce Labs Backpack']")).getText();
-        Assert.assertEquals(result, "Sauce Labs Backpack");
-        result = driver.findElement(By.xpath("//div/child::a/child::div[text()='Sauce Labs Bike Light']")).getText();
-        Assert.assertEquals(result, "Sauce Labs Bike Light");
+        Assert.assertEquals(amountOfElementsInBasket, 2, "Amount of goods doesn't much");
+        String result = driver.findElement(By.xpath(String.format(PRODUCT_IN_BASKET_XPATH, PRODUCT_NAME1))).getText();
+        Assert.assertEquals(result, PRODUCT_NAME1, PRODUCT_ABSENT_ERROR_MESSAGE);
+        result = driver.findElement(By.xpath(String.format(PRODUCT_IN_BASKET_XPATH, PRODUCT_NAME2))).getText();
+        Assert.assertEquals(result, PRODUCT_NAME2, PRODUCT_ABSENT_ERROR_MESSAGE);
         driver.quit();
     }
 
