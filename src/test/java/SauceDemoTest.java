1de28265e39b28ac9186ab2cc6b9272358539bd3
Artsiom Koptsiukh <sid93.ramsey@gmail.com>
2021-02-04 10:38:52 +0300

    refactoring imports

--- src/test/java/SauceDemoTest.java
+++ src/test/java/SauceDemoTest.java
@@ -1,10 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
 
 public class SauceDemoTest {
 
